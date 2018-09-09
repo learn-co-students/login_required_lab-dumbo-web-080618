3e7513268eac9c1e7dc98e10c02f397ca9550c86
github username <github email address>
2018-09-09 17:15:32 -0400

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,16 @@
+class SecretsController < ApplicationController
+
+  before_action :require_login
+
+  def show
+    current_user
+  end
+
+  private
+
+  def require_login
+    redirect_to login_path unless current_user
+
+  end
+
+end
